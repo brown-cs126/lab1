037264528d4b7d729a8bb3fc5b3e5eec8513d5c9
Lu, Kuang-Chen <lukuangchen1024@gmail.com>
2021-01-13 12:32:11 -0500

    minor improvements in list/

--- list/code.ml
+++ list/code.ml
@@ -1,18 +1,24 @@
 (* 1. List construction and elimination: *)
 
+(* [[]] construct an empty list  *)
 let list1 : int list = []
 
-let list2 : int list = [ 42 ]
+(* [e :: es] construct a non-empty list, where [e] is the first element and [es] are the remaining. *)
+let list2 : int list = 42 :: []
 
-let list3 : int list = [ 2; 3 ]
+(* We can construct longer lists with [[]] and [(::)]. *)
+let list3 : string list = "Welcome" :: "to" :: "CS1260" :: []
 
-let _ = assert ([ 42 ] = [ 42 ])
+(* But there is a shorthand. *)
+let list4 : string list = [ "Welcome"; "to"; "CS1260" ]
 
-let _ = assert ([ 2; 3 ] = [ 2; 3 ])
+(* The function [(@)] concatenate two lists, for examples: *)
 
 let _ = assert ([ 2; 3 ] = [ 2 ] @ [ 3 ])
 
 let _ = assert ([ 2; 3 ] = [ 2 ] @ [] @ [ 3 ])
+
+(* The assert function ensures that the two lists are the same [(=)]. *)
 
 (* Exercise: construct a list that has the integers 1 through 5 in it. Use the square bracket notation for lists. *)
 let l1 : int list = []
@@ -23,4 +29,3 @@ let l2 : int list = []
 (* Exercise: construct the same list again. This time, the following expression must appear in your answer: [2;3;4]. Use the @ operator, and do not use ::. *)
 let l3 : int list = []
 
-let _ = match list2 with [] -> "hello" | e :: _ -> string_of_int e
