b3f06cb0bc7c4c324d2d494691371b5733f0fc3c
sinanspd <sinanspd@gmail.com>
2021-01-16 17:34:49 +0300

    restructure more functions

--- more_functions/exercises.ml
+++ more_functions/exercises.ml
@@ -1,8 +1,18 @@
+let todo = failwith "unimplemented"
+
 (* Exercise: Compose*)
-let compose = 0 
+let compose = todo
 
 (* Exercise: Power of Fold *)
-let length l = 0
-let rev l = [] 
-let map f l = []
-let filter f l = []
+
+(* Function length takes in a list and returns it's length*)
+let length = todo
+
+(* Function rev reverses a given list *)
+let rev = todo
+
+(* Map takes in a function f, list l and applies f to each element in l*)
+let map = todo
+
+(* Filter removes the elements that doesn't safisy the given precidate f*)
+let filter = todo
