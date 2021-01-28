a71057fe53467f0638bfef4e3c37e6a303e1b5a4
sinanspd <sinanspd@gmail.com>
2021-01-28 00:03:03 -0500

    Update test.ml

--- recursive_functions/test.ml
+++ recursive_functions/test.ml
@@ -8,27 +8,27 @@ let assert_int_list_equal l1 l2 = assert_equal ~printer:string_of_int_list l1 l2
 let sum_tests = 
   "test sum functions"
    >::: [
-     ("" >:: fun _ -> assert_equal (sum [1, 2, 3] 5));
-     ("" >:: fun _ -> assert_equal (sum_tail [1, 2, 3] 5));
+     ("" >:: fun _ -> assert_equal (sum [1; 2; 3] 5));
+     ("" >:: fun _ -> assert_equal (sum_tail [1; 2; 3] 5));
    ]
 
 let take_while_test = 
   "test takeWhile"
   >::: [
     ("" > fun _ -> assert_int_list_equal
-     (takeWhile (fun x -> x < 4) [1, 2, 3, 4, 5, 3, 2])
+     (takeWhile (fun x -> x < 4) [1; 2; 3; 4; 5; 3; 2])
      [1, 2, 3]);
     ("" > fun _ -> assert_int_list_equal
-     (takeWhile (fun x -> x < 4) [1, 2, 3])
+     (takeWhile (fun x -> x < 4) [1; 2; 3])
      [1, 2, 3]);
     ("" > fun _ -> assert_int_list_equal
-     (takeWhile (fun x -> x < 4) [5, 6, 7])
+     (takeWhile (fun x -> x < 4) [5; 6; 7])
      []);
     ("" > fun _ -> assert_int_list_equal
      (takeWhile (fun x -> x < 4) [])
      []);
     ("" > fun _ -> assert_int_list_equal
-     (takeWhile (fun x -> x < 4) [5, 3, 2, 1])
+     (takeWhile (fun x -> x < 4) [5; 3; 2; 1])
      []);
   ]
 
